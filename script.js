6bba8985d4f924df648db615ca166a6bcde4ef6f
Nvn-One-Eyed-Eagle <naveen.phartyal2023@gmail.com>
2026-01-18 16:24:11 +0530

    minor fix

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ function givevids() {
         vid.src = url;
         vid.autoplay = false;      // important
         vid.muted = true;
-        vid.controls = false;
+        vid.controls = true;
         vid.playsInline = true;
         vid.preload = "metadata";
 
